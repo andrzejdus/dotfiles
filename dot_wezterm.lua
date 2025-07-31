2308c4a4c556be04c34b0145d6fed1fa4c21f4a0
Andrzej Duś <andrzej.dus@gmail.com>
2025-08-01 00:15:58 +0200

    add wezterm config

---
+++ dot_wezterm.lua
@@ -0,0 +1,20 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices.
+config.use_fancy_tab_bar = false
+
+-- For example, changing the initial geometry for new windows:
+config.initial_cols = 120
+config.initial_rows = 28
+
+-- or, changing the font size and color scheme.
+config.font_size = 12
+config.color_scheme = 'Solarized (dark) (terminal.sexy)'
+config.window_background_opacity = 0.95
+
+-- Finally, return the configuration to wezterm:
+return config
